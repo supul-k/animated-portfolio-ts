053cbb24af90c4a10a24bf98647e4259f4d41110
Supul Kalhara <supulkalhara7@gmail.com>
2024-08-15 12:00:01 +0530

    chore: Add initial project files and configurations

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./app.scss";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <section>Hero</section>
+        <section>Parallax</section>
+        <section>Services</section>
+        <section>Portfolio1</section>
+        <section>Portfolio2</section>
+        <section>Portfolio3</section>
+        <section>Contact</section>
+      </div>
+    </>
+  )
+}
+
+export default App
